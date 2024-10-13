e42584a15ccd22adc7b65ff191c508cdb6cf45ba
mark1995 <504391248@qq.com>
2024-10-13 19:24:16 +0800

    cache anno commit

--- jetcache-anno-api/src/main/java/com/alicp/jetcache/anno/Cached.java
+++ jetcache-anno-api/src/main/java/com/alicp/jetcache/anno/Cached.java
@@ -30,6 +30,8 @@ public @interface Cached {
      * The name is used to display statistics information and as part of key prefix when using a remote cache.
      * Do not assign same name to different @Cached annotation which with same area.
      * @return the name of the cache
+     *
+     * 设计的比较好，缓存实例命名，用来统计
      */
     String name() default CacheConsts.UNDEFINED_STRING;
 
@@ -65,6 +67,7 @@ public @interface Cached {
      * Type of the Cache instance. May be CacheType.REMOTE, CacheType.LOCAL, CacheType.BOTH.
      * Create a two level cache (local+remote) when value is CacheType.BOTH.
      * @return cache type of the method cache
+     * 缓存类型，多级缓存
      */
     CacheType cacheType() default CacheType.REMOTE;
 
@@ -72,6 +75,8 @@ public @interface Cached {
      * if cacheType is CacheType.BOTH and the remote cache supports broadcast (or BroadcastManager bean exists),
      * invalidate local cache of all process after put/remove operation.
      * @return should sync local cache
+     *
+     * 更新操作会同步到其他进程的广播
      */
     boolean syncLocal() default false;
 
@@ -80,6 +85,8 @@ public @interface Cached {
      * Use global config if the attribute value is absent, and if the global config is not defined either,
      * use CacheConsts.DEFAULT_LOCAL_LIMIT instead.
      * @return local maximal elements of the LOCAL/BOTH cache
+     *
+     * 本地缓存的最大元素数量
      */
     int localLimit() default CacheConsts.UNDEFINED_INT;
 
@@ -89,6 +96,8 @@ public @interface Cached {
      * Use global config if the attribute value is absent, and if the global config is not defined either,
      * use SerialPolicy.JAVA instead.
      * @return the serialization policy name of cache value
+     *
+     * 默认的序列化策略
      */
     String serialPolicy() default CacheConsts.UNDEFINED_STRING;
 
@@ -98,6 +107,8 @@ public @interface Cached {
      * NONE indicate do not convert, FASTJSON will use fastjson to convert key object to a string.
      * Use global config if the attribute value is absent.
      * @return convertor name of cache key
+     *
+     * key转换器，复杂的key对象转换成字符串
      */
     String keyConvertor() default CacheConsts.UNDEFINED_STRING;
 
@@ -105,12 +116,15 @@ public @interface Cached {
      * Specify the key by expression script, optional. If not specified,
      * use all parameters of the target method and keyConvertor to generate one.
      * @return an expression script which specifies key
+     *
+     * 指定缓存的key,支持spel表达式
      */
     String key() default CacheConsts.UNDEFINED_STRING;
 
     /**
      * Specify whether a null value should be cached.
      * @return whether a null value should be cached
+     * 是否缓存空值
      */
     boolean cacheNullValue() default CacheConsts.DEFAULT_CACHE_NULL_VALUE;
 
@@ -118,6 +132,8 @@ public @interface Cached {
      * Expression script used for conditioning the method caching, the cache is not
      * used when evaluation result is false.
      * Evaluation occurs before real method invocation.
+     *
+     * 什么条件下启动缓存
      */
     String condition() default CacheConsts.UNDEFINED_STRING;
 
@@ -125,6 +141,8 @@ public @interface Cached {
      * Expression script used for conditioning the method cache updating,
      * the cache updating action is vetoed when the evaluation result is false.
      * Evaluation occurs after real method invocation so we can refer <i>result</i> in script.
+     *
+     * 可以支持热点缓存
      */
     String postCondition() default CacheConsts.UNDEFINED_STRING;
 
